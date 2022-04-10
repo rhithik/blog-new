2da61eaae0f3e207f59ba8bbb70c2145964d91db
Jeff McHale <rhithiktheawesome@gmail.com>
2022-04-09 22:43:26 -0400

    started practicing with laravel. using tailwind for styling

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-white h-screen">
+    <article class="max-w-2xl mx-auto text-gray-800">
+        <h1 class="font-bold text-3xl py-6">My First Post</h1>
+        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
+    </article>
+</body>
+</html>
