a796296fb0eec5b0b3ceeb1f3736be65bc1aca0b
Jeff McHale <rhithiktheawesome@gmail.com>
2022-04-11 10:20:20 -0400

    hard coded blog posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,9 +7,19 @@
     <title>My Blog</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-white h-screen">
+<body class="bg-white h-full">
     <article class="max-w-2xl mx-auto text-gray-800">
-        <h1 class="font-bold text-3xl py-6">My First Post</h1>
+        <h1 class="font-bold text-3xl py-6"><a href="/post">My First Post</a></h1>
+        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
+    </article>
+
+    <article class="max-w-2xl mx-auto text-gray-800 border-t-2 border-gray-300 mt-6">
+        <h1 class="font-bold text-3xl py-6"><a href="/post">My Second Post</a></h1>
+        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
+    </article>
+
+    <article class="max-w-2xl mx-auto text-gray-800 border-t-2 border-gray-300 mt-6">
+        <h1 class="font-bold text-3xl py-6"><a href="/post">My Third Post</a></h1>
         <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
     </article>
 </body>
