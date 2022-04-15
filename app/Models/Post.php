38ac6e8366868f4523922c0d85bfc725b3f8239e
Jeff McHale <rhithiktheawesome@gmail.com>
2022-04-14 23:15:21 -0400

    added a Post class that allows dynamic access to all posts

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public static function all($columns = ['*'])
+    {
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(fn($file) => $file->getContents(), $files);
+    }
+
+    public static function find($slug)
+    {
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
+
+        return cache()->remember("posts.{$slug}", now()->addMinutes(20), fn() => file_get_contents($path));
+    }
+
+}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,19 +8,10 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-white h-full">
-    <article class="max-w-2xl mx-auto text-gray-800">
-        <h1 class="font-bold text-3xl py-6"><a href="/posts/my-first-post">My First Post</a></h1>
-        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
-    </article>
-
-    <article class="max-w-2xl mx-auto text-gray-800 border-t-2 border-gray-300 mt-6">
-        <h1 class="font-bold text-3xl py-6"><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
-    </article>
-
-    <article class="max-w-2xl mx-auto text-gray-800 border-t-2 border-gray-300 mt-6">
-        <h1 class="font-bold text-3xl py-6"><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p class="leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique aperiam, ullam beatae impedit error amet reiciendis reprehenderit repellat, minus saepe quisquam tempora placeat laudantium perspiciatis, magnam explicabo velit earum quibusdam. Eos necessitatibus velit, a deleniti dolorum alias eaque, quis iste accusantium fuga officiis explicabo numquam est! Illo rerum assumenda incidunt quam praesentium? Voluptate error veritatis cumque ut voluptatem magni perspiciatis consequatur officia maiores aut inventore ad laudantium cum quibusdam optio recusandae, asperiores libero. Illo.</p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article class="max-w-2xl mx-auto text-gray-800">
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
 </html>
